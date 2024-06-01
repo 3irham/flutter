028b844c978e29f75281f4ab57b8c01f6beb80f4
vreshh12 <vreshh12@gmail.com>
2024-06-01 13:09:16 +0700

    initial commit

---
+++ console_full_project/test/console_full_project_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:console_full_project/console_full_project.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
